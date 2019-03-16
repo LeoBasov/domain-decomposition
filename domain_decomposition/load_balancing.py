ae5a5fb013129f6c69f1e65e540b25143c38c4c0
Leo Basov <basov.leo@gmail.com>
2019-03-16 15:09:57 +0100

    Impelemted laod_balancing.simple

--- domain_decomposition/load_balancing.py
+++ domain_decomposition/load_balancing.py
@@ -1,4 +1,26 @@
 """Module for load balancing
 """
 
-def simple(processes, elements)
+def simple(processes, elements):
+	"""Function providing a simple load balancing
+
+	This function sorts elements to processes to a load in every element which close to the theoretical mean load.
+
+	Parameters
+	----------
+	processes : list:obj:'process'
+		List of processes which can be assigned elements
+
+	elements : list:obj:'element'
+		List of elements
+
+	Returns
+	-------
+	None
+	
+	"""
+	pass
+
+__author__ = "Leo Basov"
+__copyright__ = "Copyright (C) 2019, Leo Basov"
+__license__ = "GPLv3"
