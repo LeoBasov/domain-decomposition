f45b726af005c8015380f3bbaba7fd9e869f93c7
Leo Basov <basov.leo@gmail.com>
2019-03-16 16:23:31 +0100

    Added simple load balancing.

--- domain_decomposition/load_balancing.py
+++ domain_decomposition/load_balancing.py
@@ -1,6 +1,8 @@
 """Module for load balancing
 """
 
+import math
+
 def simple(processes, elements):
 	"""Function providing a simple load balancing
 
@@ -17,9 +19,27 @@ def simple(processes, elements):
 	Returns
 	-------
 	None
-	
+
 	"""
-	pass
+	mean_load = 0
+	max_load = 0
+	i_p = 0
+
+	for element in elements:
+		mean_load += element.load_factor
+
+	mean_load = math.ceil(mean_load/len(processes))
+
+	for element in elements:
+		if processes[i_p].load >= mean_load:
+			i_p += 1
+
+		processes[i_p].add_element(element)
+
+		if max_load < processes[i_p].load:
+			max_load = processes[i_p].load
+
+	return mean_load, max_load
 
 __author__ = "Leo Basov"
 __copyright__ = "Copyright (C) 2019, Leo Basov"
--- test/test_app.py
+++ test/test_app.py
@@ -5,24 +5,33 @@ import matplotlib.pyplot as plt
 sys.path.append('../.')
 
 import domain_decomposition.common as common
+import domain_decomposition.load_balancing as balance
 
 def main():
-	number_elements = 10
-	number_processes = 10
+	number_elements  = 10000
+	number_processes = 80
 	value_range = (0, 100)
 	loads = []
 
 	elements = []
-	processes = number_processes*[common.Process()]
+	processes = []
+
+	for i in range(number_processes):
+		process = common.Process()
+		processes.append(process)
 
 	for i in range(number_elements):
 		element = common.Element(np.random.uniform(*value_range))
 		elements.append(element)
 
+	mean_load, max_load = balance.simple(processes, elements)
+
 	for i in range(number_processes):
 		loads.append(processes[i].load)
 
 	plt.plot(loads)
+	plt.ylim(bottom=0, top=1.1*max_load)
+	plt.axhline(y=mean_load, linestyle='--', color='red', linewidth=1)
 	plt.show()
 
 if __name__ == '__main__':
